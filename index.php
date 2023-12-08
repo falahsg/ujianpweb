5f727c6896ef54187d861d4206edd1a9755193ce
falahsg <42838659+falahsg@users.noreply.github.com>
2023-12-08 22:23:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Laundry CRUD</title>
+    <title>Data Laundry A IAS FALAH</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
